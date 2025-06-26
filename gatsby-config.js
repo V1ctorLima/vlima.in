d044da2370e5ad22403d8d47c00bec56717867b9
Victor Lima <victor.lima@personio.de>
2025-06-26 12:28:13 +0200

    removing options for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
             query: `
               {
                 allMdx(
-                  filter: { 
+                  filter: {
                     internal: { contentFilePath: { regex: "/content/blog/" } }
                     frontmatter: { title: { ne: null }, slug: { ne: null } }
                   }
@@ -113,26 +113,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-H7MZ8PJ08S", // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
       },
-    },
   ],
-}; 
+};
